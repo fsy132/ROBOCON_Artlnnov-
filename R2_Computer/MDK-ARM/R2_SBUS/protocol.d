d8cf62766362bf7b93fad45166f789e586641ce9
fsy132 <2755904014@outlook.com>
2026-09-05 20:39:56 +0800

    docs: keep repository document-only

--- R2_Computer/MDK-ARM/R2_SBUS/protocol.d
+++
@@ -1,4 +0,0 @@
-r2_sbus\protocol.o: app\protocol.c
-r2_sbus\protocol.o: app\protocol.h
-r2_sbus\protocol.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-r2_sbus\protocol.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
--- R2_Computer/MDK-ARM/R2_SBUS/startup_stm32f427xx.d
+++
@@ -1,1 +0,0 @@
-r2_sbus\startup_stm32f427xx.o: startup_stm32f427xx.s
