0389fafc49661d3537b9ecf3b882791ae887a8f4
fsy132 <2755904014@outlook.com>
2026-09-05 20:39:56 +0800

    docs: keep default branch document-only

--- R2_Computer/MDK-ARM/app/action_group.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void action_group_start(uint8_t id);
-void action_group_stop(void);
-void action_group_task(void);
-void action_feedback_task(void);
--- R2_Computer/MDK-ARM/app/distance_control.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void chassis_distance_start(uint8_t mtf_index, float distance_mm);
-void chassis_distance_control_task(void);
-void side_distance_select_mtf(uint8_t index);
-void side_distance_start(uint8_t mtf_index, float distance_mm);
-void side_distance_control_task(void);
